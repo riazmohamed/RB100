6b4e29a07b321aa9e034423db4ecf0e875272c88
Riaz Mohamed <railsdevriaz@gmail.com>
2020-09-24 19:30:36 +0100

    add name.rb to Variables

---
+++ Intro_to_programming/2.Variables/name.rb
@@ -0,0 +1,8 @@
+# name.rb
+
+puts "Please enter your first name: "
+first_name = gets.chomp
+puts "Please enter your last name: "
+last_name = gets.chomp
+
+puts "Welcome #{first_name} #{last_name}!"
