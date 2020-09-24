6f90fc5d74bb688da1853cc39406636be3736afb
Riaz Mohamed <railsdevriaz@gmail.com>
2020-09-24 19:39:49 +0100

    add age.rb to Variables

---
+++ Intro_to_programming/2.Variables/age.rb
@@ -0,0 +1,15 @@
+#age.rb
+
+puts "What is your age?"
+age = gets.chomp.to_i
+
+puts "Please enter a valid number!" if age == 0
+
+puts "In 10 years you will be:"
+puts age + 10
+puts "In 20 years you will be:"
+puts age + 20
+puts "In 30 years you will be:"
+puts age + 30
+puts "In 40 years you will be:"
+puts age + 40
