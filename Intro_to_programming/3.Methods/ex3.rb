c118ea695e7e70b790d0cce643febaaa031b76fb
Riaz Mohamed <railsdevriaz@gmail.com>
2020-09-28 22:16:03 +0100

    Add ex4.rb

---
+++ Intro_to_programming/3.Methods/ex3.rb
@@ -0,0 +1,5 @@
+def muliply(num1, num2)
+  num1 * num2
+end
+
+puts muliply(10, 80)
---
+++ Intro_to_programming/3.Methods/ex4.rb
@@ -0,0 +1,7 @@
+def scream(words)
+  words = words + "!!!!"
+  return # the method stops executing here and returns nil 
+  puts words
+end
+
+scream("Yippeee") # this willr return a nil
