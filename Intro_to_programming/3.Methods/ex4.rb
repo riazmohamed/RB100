76bc45409b485c07f58db287a226321155023fb2
Riaz Mohamed <railsdevriaz@gmail.com>
2020-09-28 22:17:36 +0100

    Edit ex4.rb

--- Intro_to_programming/3.Methods/ex4.rb
+++ Intro_to_programming/3.Methods/ex4.rb
@@ -1,7 +1,6 @@
 def scream(words)
   words = words + "!!!!"
-  return # the method stops executing here and returns nil 
   puts words
 end
 
-scream("Yippeee") # this willr return a nil
+scream("Yippeee") 
