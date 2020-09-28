405d577194be913384fe2bb6e169ff2b8eed6e47
Riaz Mohamed <railsdevriaz@gmail.com>
2020-09-28 21:41:04 +0100

    Add method call as arguments

---
+++ Intro_to_programming/3.Methods/method_call.rb
@@ -0,0 +1,15 @@
+def add(a, b)
+  a + b
+end
+
+def subtract(a, b)
+  a - b
+end
+
+def multiply(num1, num2)
+  num1 * num2
+end
+
+p multiply(add(20,45), subtract(90, 20))
+
+p add(subtract(80, 10), multiply(subtract(20, 6), add(30, 5)))
---
+++ Intro_to_programming/3.Methods/method_chain.rb
@@ -0,0 +1,7 @@
+def add_three(new_value)
+    new_value + 3
+    puts new_value
+    new_value
+end
+
+add_three(3).times { puts "Hello World" }
