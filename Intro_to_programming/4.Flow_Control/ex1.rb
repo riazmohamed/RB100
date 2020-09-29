8dafdd7e72e5ff4130444f26c49ce232dd157ab6
Riaz Mohamed <railsdevriaz@gmail.com>
2020-09-29 17:12:44 +0100

    git edit ex2.rb

--- Intro_to_programming/4.Flow_Control/ex1.rb
+++ Intro_to_programming/4.Flow_Control/ex1.rb
@@ -1,5 +1,5 @@
 p (32 * 4) >= 129 # => false
 p false != !true # => false
-p true == 4 # => false
+p true == 4 # => false - 4 is a truthy value but is not equal to the boolean true
 p false == (847 == '847') # => true
 p (!true || (!(100 / 5) == 20) || ((328 / 4) == 82)) || false # => true
--- Intro_to_programming/4.Flow_Control/ex2.rb
+++ Intro_to_programming/4.Flow_Control/ex2.rb
@@ -1,6 +1,5 @@
 def capitals(words)
-  return words.upcase if words.length > 10
-  words
+  words.length > 10 ? words.upcase : words
 end
 
 puts capitals("Helloooooooooooooooo")
