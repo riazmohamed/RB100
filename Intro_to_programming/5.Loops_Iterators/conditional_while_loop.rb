b1276c8ef682a7acfe53fef391a99c08728a4d8b
Riaz Mohamed <railsdevriaz@gmail.com>
2020-09-30 20:40:27 +0100

    practicing iterators

---
+++ Intro_to_programming/5.Loops_Iterators/conditional_while_loop.rb
@@ -0,0 +1,8 @@
+x = 0
+
+while x <= 10
+  if x.odd? # method to check if the variable is odd.
+    puts x
+  end
+  x += 1 # x is incremented by 1 before the next iteration
+end
---
+++ Intro_to_programming/5.Loops_Iterators/practice_each.rb
@@ -0,0 +1,7 @@
+names = ['Bob', 'Joe', 'Steven', 'Janice', 'Susan', 'Helen']
+x = 1
+
+names.each do |name|
+  puts "#{x}. #{name}"
+  x += 1
+end
