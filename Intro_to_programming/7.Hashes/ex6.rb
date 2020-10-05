544c0e2a22fae473fd0f7da3b2f4a6843d63fac8
Riaz Mohamed <railsdevriaz@gmail.com>
2020-10-05 11:36:31 +0100

    add ex8.rb

---
+++ Intro_to_programming/7.Hashes/ex6.rb
@@ -0,0 +1,3 @@
+x = "hi there"
+p my_hash = {x: "some value"} # key data tyoe here is a symbol
+p my_hash2 = {x => "some value"} # key here is a local varible "x"  refencing the string "hi there"
---
+++ Intro_to_programming/7.Hashes/ex8.rb
@@ -0,0 +1,18 @@
+words =  ['demo', 'none', 'tied', 'evil', 'dome', 'mode', 'live',
+          'fowl', 'veil', 'wolf', 'diet', 'vile', 'edit', 'tide',
+          'flow', 'neon']
+
+new_hsh = Hash.new
+split_word = ""
+hash_key = ""
+words.each do |ele|
+  word = ele.split("").sort.join("")
+  if
+    new_hsh.has_key?(word)
+    new_hsh[word] << ele
+  elsif new_hsh.has_key?(word) == false
+      new_hsh[word] = [ele]
+  end
+end
+
+new_hsh.values.each { |val| p val }
