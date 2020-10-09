e8905d670efcf3c66f761f6414223f4eeefd03b6
Riaz Mohamed <railsdevriaz@gmail.com>
2020-10-09 10:45:49 +0100

    add ex10.rb

---
+++ Intro_to_programming/Exercise_from_Book/ex10.rb
@@ -0,0 +1,11 @@
+# hash with arrays as values
+
+hash = {
+  k1: [1, 2, 3, 4, 5],
+  k2: [5, 4, 3, 2, 1]
+}
+
+arr = [{ colour1: "blue" }, { colour2: "red" }, { colour3: "green" }]
+
+puts hash
+puts arr
