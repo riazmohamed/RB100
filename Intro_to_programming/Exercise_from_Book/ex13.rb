f9ffac13d3133cd3741a099e2821f852310ca5c7
Riaz Mohamed <railsdevriaz@gmail.com>
2020-10-09 21:22:41 +0100

    Modify ex13.rb

--- Intro_to_programming/Exercise_from_Book/ex13.rb
+++ Intro_to_programming/Exercise_from_Book/ex13.rb
@@ -1,6 +1,7 @@
 arr = ['snow', 'winter', 'ice', 'slippery', 'salted roads', 'white trees']
-
 arr.delete_if { |str| str.start_with?('s') }
-
 p arr
- 
+
+arr = ['snow', 'winter', 'ice', 'slippery', 'salted roads', 'white trees']
+arr.delete_if { |str| str.start_with?('s', "w") }
+p arr
