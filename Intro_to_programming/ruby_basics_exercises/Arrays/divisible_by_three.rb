1c6db820fe3764dfc08477ec6ca1bd7d12f74fbc
Riaz Mohamed <railsdevriaz@gmail.com>
2020-12-30 20:49:12 +0000

    House keeping

---
+++ Intro_to_programming/ruby_basics_exercises/Arrays/divisible_by_three.rb
@@ -0,0 +1,2 @@
+numbers = [5, 9, 21, 26, 39]
+p divisible_by_three = numbers.select { |num| num % 3 == 0 }
---
+++ Intro_to_programming/ruby_basics_exercises/Arrays/doubled.rb
@@ -0,0 +1,2 @@
+numbers = [1, 2, 3, 4, 5]
+p doubled_numbers = numbers.map { |num| num * 2 }
